9865c51b71c71ac1bb88e83a1ace9356176e0f7d
nola360 <adaramolajr@gmail.com>
2020-03-02 21:16:14 -0500

    added scroll height method to content body

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const plusSign = document.querySelectorAll('.accordion-item-header');
 plusSign.forEach(header => {
   header.addEventListener('click', event => {
     header.classList.toggle('active');
+    const accordionBody = header.nextElementSibling;
+    if (header.classList.contains('active')) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+    } else {
+      accordionBody.style.maxHeight = 0;
+    }
   })
-
-
 })
+
+// ScrollHeight property return the entire height of an element in px including the height that is not visible due to overflow
