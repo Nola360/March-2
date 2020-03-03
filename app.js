fa72916896cfa1f4e546377d496da8bd1fb48dad
nola360 <adaramolajr@gmail.com>
2020-03-02 21:44:33 -0500

    added more accordion item headers

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const plusSign = document.querySelectorAll('.accordion-item-header');
 
 plusSign.forEach(header => {
   header.addEventListener('click', event => {
+    // checks if accordion item body is open & closes upon clicking another
+    const openBody = document.querySelector('.accordion-item-header.active');
+    console.log(openBody);
+    if (openBody && openBody !== header) {
+      openBody.classList.toggle('active');
+      openBody.nextElementSibling.style.maxHeight = 0;
+    }
+
+    // adds active class to accordion item header to open
     header.classList.toggle('active');
     const accordionBody = header.nextElementSibling;
     if (header.classList.contains('active')) {
@@ -12,4 +21,4 @@ plusSign.forEach(header => {
   })
 })
 
-// ScrollHeight property return the entire height of an element in px including the height that is not visible due to overflow
+// scrollHeight property return the entire height of an element in px including the height that is not visible due to overflow
